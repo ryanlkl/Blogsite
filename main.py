07ed2c6c0913a05620cc1c8bb15e7c9655f79b23
Ryan <121324625+ryanlkl@users.noreply.github.com>
2023-09-11 17:50:34 +0100

    Ignoring files

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+from decouple import config
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
 
@@ -28,7 +29,7 @@ This will install the packages from the requirements.txt for this project.
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = config("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
